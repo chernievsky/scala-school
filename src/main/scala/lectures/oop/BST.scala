136ff8d68b1b043b4dc9cf19ee4204da14311a7b
chernievsky <vitaly.chernievsky@gmail.com>
2016-08-01 14:29:29 +0300

    homework 4 (incomplete: no toSting() and tests).

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -30,15 +30,61 @@ trait BST {
   def add(newValue: Int): BST
 
   def find(value: Int): Option[BST]
+
+  def fold(aggregator: Int)(f: (Int, Int) =>(Int)): Int
 }
 
 case class BSTImpl(value: Int,
                    left: Option[BSTImpl] = None,
                    right: Option[BSTImpl] = None) extends BST {
 
-  def add(newValue: Int): BST = ???
+  def add(newValue: Int): BSTImpl = {
+    if (value == newValue) {
+      this
+    } else if (newValue < value) {
+      this match {
+        case BSTImpl(_, Some(l), _) => BSTImpl(value, Some(l.add(newValue)), right)
+        case _                      => BSTImpl(value, Some(BSTImpl(newValue, None, None)), right)
+      }
+    } else {
+      this match {
+        case BSTImpl(_, _, Some(r)) => BSTImpl(value, left, Some(r.add(newValue)))
+        case _                      => BSTImpl(value, left, Some(BSTImpl(newValue, None, None)))
+      }
+    }
+  }
 
-  def find(value: Int): Option[BST] = ???
+  def find(value: Int): Option[BST] = {
+    if (value == this.value) {
+      Some(this)
+    } else if (value < this.value) {
+      this match {
+        case BSTImpl(_, Some(l), _) => l.find(value)
+        case _                      => None
+      }
+    } else {
+      this match {
+        case BSTImpl(_, _, Some(r)) => r.find(value)
+        case _                      => None
+      }
+    }
+  }
+
+  def fold(aggregator: Int)(f: (Int, Int) =>(Int)): Int = {
+    val aggThis = f(aggregator, value)
+
+    val aggLeft = left match {
+      case Some(l) => l.fold(aggThis)(f)
+      case _       => aggThis
+    }
+
+    val aggRight = right match {
+      case Some(r) => r.fold(aggLeft)(f)
+      case _       => aggLeft
+    }
+
+    aggRight
+  }
 
   // override def toString() = ???
 
@@ -56,15 +102,17 @@ object TreeTest extends App {
 
   // Generate huge tree
   val root: BST = BSTImpl(maxValue / 2)
-  val tree: BST = ??? // generator goes here
+  val tree: BST = (1 until nodesCount).foldLeft(root)((t, _) => t.add((Math.random() * maxValue).toInt))
 
   // add marker items
   val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
 
   // check that search is correct
   require(testTree.find(markerItem).isDefined)
-  require(testTree.find(markerItem).isDefined)
-  require(testTree.find(markerItem).isDefined)
+  require(testTree.find(markerItem2).isDefined)
+  require(testTree.find(markerItem3).isDefined)
 
   println(testTree)
+  println(testTree.fold(0)((agg, x) => agg + x))
+  println(testTree.fold(1)((agg, x) => agg * x))
 }
