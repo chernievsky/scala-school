145606822793be07424e863584e462a9466cd7db
Vitaly Chernievsky <vitaly.chernievsky@gmail.com>
2016-09-07 19:27:00 +0300

    homework 4: BST.toString().

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -34,21 +34,27 @@ trait BST {
   def fold(aggregator: Int)(f: (Int, Int) =>(Int)): Int
 }
 
+case class BSTText(nodeLine: String, linesBelow: List[String])
+
 case class BSTImpl(value: Int,
                    left: Option[BSTImpl] = None,
                    right: Option[BSTImpl] = None) extends BST {
 
-  def add(newValue: Int): BSTImpl = {
+  def add(newValue: Int): BST = {
+    addImpl(newValue)
+  }
+
+  private def addImpl(newValue: Int): BSTImpl = {
     if (value == newValue) {
       this
     } else if (newValue < value) {
       this match {
-        case BSTImpl(_, Some(l), _) => BSTImpl(value, Some(l.add(newValue)), right)
+        case BSTImpl(_, Some(l), _) => BSTImpl(value, Some(l.addImpl(newValue)), right)
         case _                      => BSTImpl(value, Some(BSTImpl(newValue, None, None)), right)
       }
     } else {
       this match {
-        case BSTImpl(_, _, Some(r)) => BSTImpl(value, left, Some(r.add(newValue)))
+        case BSTImpl(_, _, Some(r)) => BSTImpl(value, left, Some(r.addImpl(newValue)))
         case _                      => BSTImpl(value, left, Some(BSTImpl(newValue, None, None)))
       }
     }
@@ -86,15 +92,51 @@ case class BSTImpl(value: Int,
     aggRight
   }
 
-  // override def toString() = ???
+  override def toString(): String = {
+    val text = getText()
+    text.nodeLine + "\n" + text.linesBelow.mkString("\n")
+  }
+
+  private def spaces(length: Int): String = "".padTo(length, ' ')
+
+  private def padLeft(neededLength: Int, line: String): String = {
+    if (line.length >= neededLength)
+      line
+    else
+      line.reverse.padTo(neededLength, ' ').reverse
+  }
+
+  private def getText(): BSTText = {
+    this match {
+      case BSTImpl(_, None, None)       => BSTText(this.value.toString, List())
+
+      case BSTImpl(v, Some(l), Some(r)) =>
+        val leftText = l.getText()
+        val rightText = r.getText()
+        val nodeLine = spaces(leftText.nodeLine.length) + v + spaces(rightText.nodeLine.length)
+        val bottomLine = leftText.nodeLine + spaces(v.toString.length) + rightText.nodeLine
+        val zippedLines = leftText.linesBelow.zipAll(rightText.linesBelow, "", "").map({ case (ll, rl) => ll + padLeft(nodeLine.length - ll.length, rl) })
+        BSTText(nodeLine, bottomLine +: zippedLines)
+
+      case BSTImpl(v, Some(l), None)    =>
+        val leftText = l.getText()
+        val nodeLine = spaces(leftText.nodeLine.length) + v
+        BSTText(nodeLine, (leftText.nodeLine +: leftText.linesBelow).map(line => line.padTo(nodeLine.length, ' ')))
+
+      case BSTImpl(v, None, Some(r))    =>
+        val rightText = r.getText()
+        val nodeLine = v + spaces(rightText.nodeLine.length)
+        BSTText(nodeLine, (rightText.nodeLine +: rightText.linesBelow).map(line => padLeft(nodeLine.length, line)))
+    }
+  }
 
 }
 
 object TreeTest extends App {
 
   val sc = new java.util.Scanner(System.in)
-  val maxValue = 110000
-  val nodesCount = sc.nextInt()
+  val maxValue = 200
+  val nodesCount = 1 //sc.nextInt()
 
   val markerItem = (Math.random() * maxValue).toInt
   val markerItem2 = (Math.random() * maxValue).toInt
@@ -105,7 +147,8 @@ object TreeTest extends App {
   val tree: BST = (1 until nodesCount).foldLeft(root)((t, _) => t.add((Math.random() * maxValue).toInt))
 
   // add marker items
-  val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
+  val testTree = tree.add(100).add(82).add(55).add(153).add(143).add(149).add(183).add(159).add(154).add(180).add(151).add(181)
+      .add(markerItem).add(markerItem2).add(markerItem3)
 
   // check that search is correct
   require(testTree.find(markerItem).isDefined)
