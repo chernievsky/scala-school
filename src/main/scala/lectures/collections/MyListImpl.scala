e136603847e05ae1f30d5960cd80b2b6cd0bf962
Vitaly Chernievsky <vitaly.chernievsky@gmail.com>
2017-05-16 22:05:26 +0300

    homework: type parameters (MyList).

--- src/main/scala/lectures/collections/MyListImpl.scala
+++ src/main/scala/lectures/collections/MyListImpl.scala
@@ -1,5 +1,7 @@
 package lectures.collections
 
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
 /**
   * Представим, что по какой-то причине Вам понадобилась своя обертка над списком целых чисел List[Int]
   *
@@ -17,29 +19,41 @@ package lectures.collections
   */
 object MyListImpl extends App {
 
-  case class MyList(data: List[Int]) {
+  case class MyList[T, S <: Seq[T]](data: Seq[T]) {
 
-    def flatMap(f: (Int => MyList)) =
-      MyList(data.flatMap(inp => f(inp).data))
+    def flatMap(f: (T => MyList[T, S])): MyList[T, S] =
+      MyList[T, S](data.flatMap(inp => f(inp).data))
 
-    def map(f: (Int => Int)) = {
-      flatMap(inp => MyList(List(f(inp))))
+    def map(f: (T => T)): MyList[T, S] = {
+      flatMap(inp => MyList[T, S](List(f(inp))))
     }
 
-    def foldLeft(acc: Int)(f: ((Int, Int)) => Int): Int = {
-      data match {
-        case Nil          => acc
-        case head :: tail => MyList(tail).foldLeft(f((acc, head)))(f)
-      }
+    def foldLeft(acc: T)(f: ((T, T)) => T): T = {
+      if (data.isEmpty) acc
+      else MyList[T, S](data.tail).foldLeft(f((acc, data.head)))(f)
+//      data match {
+//        case Nil          => acc
+//        case head :: tail => MyList[T, S](tail).foldLeft(f((acc, head)))(f)
+//      }
     }
 
-    def filter(f: Int => Boolean) = {
-      val filterHelper: PartialFunction[Int, MyList] = {
-        case x if f(x) => MyList(List(x))
-        case _         => MyList(Nil)
+    def filter(f: T => Boolean): MyList[T, S] = {
+      val filterHelper: PartialFunction[T, MyList[T, S]] = {
+        case x if f(x) => MyList[T, S](List(x))
+        case _         => MyList[T, S](Nil)
       }
       flatMap(filterHelper)
     }
+  }
+
+  class MyListBuffer[T](data: ListBuffer[T]) extends MyList[T, ListBuffer[T]](data)
+  object MyListBuffer {
+    def apply[T](data: ListBuffer[T]) = new MyListBuffer[T](data)
+  }
+
+  class MyIndexedList[T](data: IndexedSeq[T]) extends MyList[T, IndexedSeq[T]](data)
+  object MyIndexedList {
+    def apply[T](data: IndexedSeq[T]) = new MyIndexedList[T](data)
   }
 
   require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
@@ -47,4 +61,11 @@ object MyListImpl extends App {
   require(MyList(List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 21)
   require(MyList(Nil).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
 
+  require(MyList[Int, List[Int]](List(1, 2, 3, 4, 5, 6)).map(p => p * 2).data == List(2, 4, 6, 8, 10, 12))
+  require(MyList[Long, ListBuffer[Long]](ListBuffer(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
+  require(MyList[Int, List[Int]](List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 21)
+  require(MyList[Float, IndexedSeq[Float]](ArrayBuffer.empty[Float]).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
+
+  require(MyListBuffer[Long](ListBuffer(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
+  require(MyIndexedList[Float](ArrayBuffer.empty[Float]).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
 }
