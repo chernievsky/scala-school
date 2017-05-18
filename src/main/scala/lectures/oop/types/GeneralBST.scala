e94d7917b8bd378f659978b95383ad2263ae5f4b
Vitaly Chernievsky <vitaly.chernievsky@gmail.com>
2017-05-18 19:10:39 +0300

    homework: type parameters & implicits (GeneralBST).

--- src/main/scala/lectures/oop/types/GeneralBST.scala
+++ src/main/scala/lectures/oop/types/GeneralBST.scala
@@ -1,5 +1,7 @@
 package lectures.oop.types
 
+import lectures.matching.SortingStuff.Watches
+
 /**
   * Модифицируйте реализацию BSTImpl из предыдущего задания.
   * Используя тайп параметры и паттерн Type Class, реализуйте GeneralBSTImpl таким образом,
@@ -13,23 +15,76 @@ package lectures.oop.types
   * * * * Watches из задачи SortStuff. Большими считаются часы с большей стоимостью
   */
 
-trait GeneralBST {
-  //val value: T
-  val left: Option[GeneralBST]
-  val right: Option[GeneralBST]
+trait GeneralBST[T] {
+  val value: T
+  val left: Option[GeneralBST[T]]
+  val right: Option[GeneralBST[T]]
 
-  def add(newValue: Int): GeneralBST
+  def add(newValue: T): GeneralBST[T]
 
-  def find(value: Int): Option[GeneralBST]
+  def find(value: T): Option[GeneralBST[T]]
 }
 
-class GeneralBSTImpl extends GeneralBST {
+class GeneralBSTImpl[T: Ordering](val value: T,
+                                  val left: Option[GeneralBST[T]] = None,
+                                  val right: Option[GeneralBST[T]] = None) extends GeneralBST[T] {
+  val ord: Ordering[T] = implicitly[Ordering[T]]
   //override val value = _
+  //override val left: Option[GeneralBST[T]] = ???
+  //override val right: Option[GeneralBST[T]] = ???
 
-  override def find(value: Int): Option[GeneralBST] = ???
+  override def find(value: T): Option[GeneralBST[T]] = {
+    if (ord.lt(value, this.value))
+      left.flatMap(_.find(value))
+    else if (ord.gt(value, this.value))
+      right.flatMap(_.find(value))
+    else
+      Some(this)
+  }
 
-  override def add(newValue: Int): GeneralBST = ???
+  override def add(newValue: T): GeneralBST[T] = {
+    if (this.value == newValue)
+      this
+    else if (ord.lt(newValue, this.value))
+      new GeneralBSTImpl[T](this.value, left.map(_.add(newValue)).orElse(Some(new GeneralBSTImpl[T](newValue))), right)
+    else
+      new GeneralBSTImpl[T](this.value, left, right.map(_.add(newValue)).orElse(Some(new GeneralBSTImpl[T](newValue))))
+  }
 
-  override val left: Option[GeneralBST] = ???
-  override val right: Option[GeneralBST] = ???
+  override def toString: String = "{" + left.getOrElse("-").toString + "." + value + "." + right.getOrElse("-").toString + "}"
+}
+
+object BSTGenerator extends App {
+  val maxValue = 200
+
+  trait Gen[T] {
+    def apply(): T
+  }
+
+  implicit val genInt: Gen[Int] = () => (Math.random() * maxValue).toInt
+  implicit val genFloat: Gen[Float] = () => (Math.random() * maxValue).toFloat
+  implicit val genString: Gen[String] = () => scala.util.Random.alphanumeric.take(5).mkString
+  implicit val genWatches: Gen[Watches] = () => Watches(genString(), genFloat())
+
+  def buildBST[T: Ordering](cnt: Int)(implicit evidence: Gen[T]): GeneralBST[T] = {
+    val gen = implicitly[Gen[T]]
+    val root: GeneralBST[T] = new GeneralBSTImpl[T](gen())
+
+    (1 until cnt).foldLeft(root)((t, _) => t.add(gen()))
+  }
+
+  val sc = new java.util.Scanner(System.in)
+  val nodesCount = sc.nextInt()
+
+  val intTree: GeneralBST[Int] = buildBST[Int](nodesCount)
+  val floatTree: GeneralBST[Float] = buildBST[Float](nodesCount)
+  val stringTree: GeneralBST[String] = buildBST[String](nodesCount)
+
+  implicit val watchesOrdering: Ordering[Watches] = Ordering.by(_.cost) // http://stackoverflow.com/questions/25733902/scala-implicit-ordering
+  val watchesTree: GeneralBST[Watches] = buildBST[Watches](nodesCount)
+
+  println(intTree)
+  println(floatTree)
+  println(stringTree)
+  println(watchesTree)
 }
